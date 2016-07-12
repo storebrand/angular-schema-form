d4e75427d9b26860ae87e5fbe1ff823d0a6ddc99
Navka <O7J@storebrand.com>
2016-07-12 16:26:41 +0200

    add restriction by length and key code in number field

--- src/directives/numberFormat.js
+++ src/directives/numberFormat.js
@@ -45,6 +45,36 @@ angular.module('schemaForm').directive('numberFormat', function() {
         val = addSpaces(val.toString());
         return val;
       });
+      
+      elm.bind('keydown', function (e) {
+
+        var maxLength = scope.$eval(attr.modelMaxLength);
+
+        // Allow: backspace, delete, tab, escape, enter and .
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+            // Allow: Ctrl+A
+            (e.keyCode == 65 && e.ctrlKey === true) ||
+            // Allow: Ctrl+C
+            (e.keyCode == 67 && e.ctrlKey === true) ||
+            // Allow: Ctrl+X
+            (e.keyCode == 88 && e.ctrlKey === true) ||
+            // Allow: Ctrl+V
+            (e.keyCode == 86 && e.ctrlKey === true) ||
+            // Allow: home, end, left, right
+            (e.keyCode >= 35 && e.keyCode <= 39)) {
+          // let it happen, don't do anything
+          return;
+        }
+        // Ensure that it is a number and stop the keypress
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+          e.preventDefault();
+        }
+
+        if (angular.isDefined(maxLength) &&  elm.val().length >= maxLength) {
+          e.preventDefault();
+        }
+      });
+      
       elm.bind('blur', function() {
         scope.$apply(function() {
           var formatter, viewValue, _i, _len, _ref;
